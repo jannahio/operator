9df862aeb8efbf745ed65b58e402f27301e86eb9
Osman Jalloh <osman.jalloh@gmail.com>
2023-03-25 20:56:33 -0700

    Ongoing efforts to make Ubuntu the default dev environment:working

--- development/boot_vm.sh
+++ development/boot_vm.sh
@@ -8,6 +8,6 @@ python3-pip \
 virtualenv
 multipass exec jannah-charm-operator-amd64 -- sudo snap install yq
 #multipass exec jannah-charm-operator-amd64 -- ls -lrt /home/ubuntu/
-#multipass exec jannah-charm-operator-amd64 -- pip3 install --user -r /home/ubuntu/IdeaProjects/operator/charm/src/requirements.txt
+#multipass exec jannah-charm-operator-amd64 -- pip3 install --user -r /home/ubuntu/IdeaProjects/operator/charm/requirements.txt
 #multipass exec jannah-charm-operator-amd64 -- cd /home/ubuntu/IdeaProjects/operator && make jannah-config
 #multipass shell jannah-charm-operator-amd64
